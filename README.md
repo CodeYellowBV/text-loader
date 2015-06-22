0ab67398fa2377cf8f22d6a253a22a6f888a0beb
Kees Kluskens <kees@webduck.nl>
2015-06-22 11:31:42 +0200

    Insert a comment with the filename in it for html files.

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# raw loader for webpack
+# text loader for webpack
+Works the same as [raw-loader](https://github.com/webpack/raw-loader), but prepends a comment with the relative path in it (e.g. `<-- view/user/edit.html -->`) for HTML files. This comes in handy when developing a complex single page app that nests template files.
 
 ## Installation
 
-`npm install raw-loader`
+`npm install CodeYellowBV/text-loader`
 
 ## Usage
 
 ``` javascript
-var fileContent = require("raw!./file.txt");
-// => returns file.txt content as string
+var fileContent = require("text!./file.txt");
+// => returns file.txt content as string (no comment prepended).
+```
+
+``` javascript
+var fileContent = require("text!./file.html");
+// => returns file.html content as string, with <!-- path/to/file.html --> prepended.
 ```
 
 [Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
+var loaderUtils = require('loader-utils');
+var path = require('path');
+
+// Get the root path (where the webpack config file also should be).
+var appRoot = path.join(__dirname, '../..');
+
+// Prepend html files with a comment that has the filename in it.
+// This comes in handy when debugging templates via the DevTools.
+function convertContent(content, filename) {
+    if (/.html$/.test(filename)) {
+        return "\n<!-- " + filename + "  -->\n" + content;
+    }
+
+    return content;
+}
+
 module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	this.value = content;
-	return "module.exports = " + JSON.stringify(content);
+    // Get the absolute path to the required file and make it relative to our app root.
+    var filename = loaderUtils.getRemainingRequest(this);
+    var filenameRelative = path.relative(appRoot, filename);
+
+    var convertedContent = convertContent(content, filenameRelative);
+
+    this.cacheable && this.cacheable();
+    this.value = content;
+
+    return "module.exports = " + JSON.stringify(convertedContent);
 }
